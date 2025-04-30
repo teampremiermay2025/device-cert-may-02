8b6b8a9b60c21671d8804218191ed4dfd0b07e2f
Amit Mehta <ammehta2@in.ibm.com>
2025-04-30 12:21:45 -0400

    Adding changes

--- src/components/ViewCertificationModal.tsx
+++ src/components/ViewCertificationModal.tsx
@@ -543,18 +543,6 @@ export const ViewCertificationModal: FC<ViewCertificationModalProps> = ({
                                 />
                                 <div>
                                   <h4 className="font-medium">{task.name}
-                                    {/* {task.name === '2005.2 (PTN-20006 Monitoring Results)' && ( */}
-                                      <button
-                                        onClick={handleAITestCaseClick}
-                                        className="ml-2 p-1 bg-blue-500 text-white rounded hover:bg-blue-600 flex items-center"
-                                        title="Generate AI Test Case"
-                                      >
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                                        </svg>
-                                        <span className="inline-block align-middle">Generate Test Cases</span>
-                                      </button>
-                                    {/* )} */}
                                   </h4>
                                   {task.description && (
                                     <p className="text-sm text-gray-600 mt-1">
@@ -611,7 +599,7 @@ export const ViewCertificationModal: FC<ViewCertificationModalProps> = ({
                         Add Comment
                       </button>
 
-                      {selectedTask.name === '2005.2 (PTN-20006 Monitoring Results)' && (
+                    
                       <button
                         onClick={handleAITestCaseClick}
                         className="ml-2 mt-4 p-1 bg-pink-500 text-white rounded hover:bg-blue-600 flex items-center"
@@ -621,7 +609,7 @@ export const ViewCertificationModal: FC<ViewCertificationModalProps> = ({
                         <SparklesIcon className="w-5 h-5 mr-2 text-white" />
                         <span className="inline-block align-middle">Generate Test Cases</span>
                       </button>
-                    )}
+
                     </div>
                     <div className="space-y-4">
                       {editedTask.comments.map((comment) => (
